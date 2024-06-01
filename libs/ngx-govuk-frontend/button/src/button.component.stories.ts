84fe9da55621b0fe72c482746d23e6e663bd383f
Jonny Heavey <jonnyheavey@gmail.com>
2024-06-01 12:34:22 +0100

    setup storybook

---
+++ libs/ngx-govuk-frontend/button/src/button.component.stories.ts
@@ -0,0 +1,41 @@
+import { InputSignal } from '@angular/core';
+import {
+  argsToTemplate,
+  type Meta,
+  type StoryFn,
+  type StoryObj,
+} from '@storybook/angular';
+import { GovUKButtonColorType, GovUKButtonComponent } from './button.component';
+
+const meta: Meta<GovUKButtonComponent> = {
+  component: GovUKButtonComponent,
+  title: 'GovUKButtonComponent',
+  argTypes: {
+    color: {
+      options: ['primary', 'secondary', 'warning', 'inverse'],
+      control: { type: 'select' },
+    },
+  },
+};
+export default meta;
+type Story = StoryObj<GovUKButtonComponent>;
+
+const ButtonTemplate: StoryFn<GovUKButtonComponent> = (
+  args: GovUKButtonComponent
+) => ({
+  props: { ...args },
+  template: `<ngx-govuk-button ${argsToTemplate(args)}>
+    Hello world
+  </ngx-govuk-button>`,
+});
+
+const defaultArgs = {
+  color: 'primary' as unknown as InputSignal<GovUKButtonColorType>,
+  disabled: false as unknown as InputSignal<boolean>,
+  start: false as unknown as InputSignal<boolean>,
+};
+
+export const Primary: Story = {
+  render: ButtonTemplate,
+  args: defaultArgs,
+};
