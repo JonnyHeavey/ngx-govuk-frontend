1b301f56f4a2164b6dc38be115c5dc9eecfa7878
Jonny Heavey <jonnyheavey@gmail.com>
2024-06-01 12:34:22 +0100

    design system install and test app setup

--- apps/test-app/src/app/app.component.ts
+++ apps/test-app/src/app/app.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { NgxGovukFrontendComponent } from 'ngx-govuk-frontend';
 
 @Component({
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, NgxGovukFrontendComponent],
   selector: 'ngx-govuk-frontend-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'test-app';
-}
+export class AppComponent {}
