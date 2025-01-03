de0d6098a1bf1571d5f6119afde593124107136e
Jonny Heavey <jonnyheavey@gmail.com>
2025-01-03 20:32:19 +0000

    tidy button group

--- libs/ngx-govuk-frontend/button/src/lib/button-group/button-group.component.ts
+++ libs/ngx-govuk-frontend/button/src/lib/button-group/button-group.component.ts
@@ -1,10 +1,27 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { GovUKButtonComponent } from '../button/button.component';
 
+/**
+ * A component that groups GOV.UK buttons together with consistent spacing.
+ *
+ * This component implements the GOV.UK Design System button group pattern.
+ * It provides consistent spacing between multiple buttons or links that are grouped together.
+ *
+ * @example
+ * ```html
+ * <ngx-govuk-button-group>
+ *   <ngx-govuk-button>Save and continue</ngx-govuk-button>
+ *   <ngx-govuk-button color="secondary">Cancel</ngx-govuk-button>
+ * </ngx-govuk-button-group>
+ * ```
+ *
+ * The component accepts `ngx-govuk-button` components and elements with the `govuk-link` class
+ * as content through content projection.
+ *
+ * @see {@link https://design-system.service.gov.uk/components/button/#button-group GOV.UK Button group}
+ */
 @Component({
   selector: 'ngx-govuk-button-group',
   standalone: true,
-  imports: [GovUKButtonComponent],
   templateUrl: './button-group.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
