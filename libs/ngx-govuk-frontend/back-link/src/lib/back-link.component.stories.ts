87e72a8e4a22fceae703aa285d9f59dd9f642fd0
Jonny Heavey <jonnyheavey@gmail.com>
2025-01-03 20:30:03 +0000

    backlink improvements

--- libs/ngx-govuk-frontend/back-link/src/lib/back-link.component.stories.ts
+++ libs/ngx-govuk-frontend/back-link/src/lib/back-link.component.stories.ts
@@ -1,4 +1,9 @@
-import { type Meta, type StoryFn, type StoryObj } from '@storybook/angular';
+import {
+  argsToTemplate,
+  type Meta,
+  type StoryFn,
+  type StoryObj,
+} from '@storybook/angular';
 import { GovUKBackLinkComponent } from './back-link.component';
 
 const meta: Meta<GovUKBackLinkComponent> = {
@@ -8,13 +13,14 @@ const meta: Meta<GovUKBackLinkComponent> = {
 export default meta;
 type Story = StoryObj<GovUKBackLinkComponent>;
 
-const Template: StoryFn<GovUKBackLinkComponent> = (
-  args: GovUKBackLinkComponent,
-) => ({
+const Template: StoryFn<GovUKBackLinkComponent> = (args) => ({
   props: { ...args },
-  template: `<ngx-govuk-back-link />`,
+  template: `<ngx-govuk-back-link ${argsToTemplate(args)} />`,
 });
 
 export const Primary: Story = {
   render: Template,
+  args: {
+    inverse: false,
+  },
 };
--- libs/ngx-govuk-frontend/back-link/src/lib/back-link.component.ts
+++ libs/ngx-govuk-frontend/back-link/src/lib/back-link.component.ts
@@ -1,9 +1,34 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { NgClass } from '@angular/common';
+import { ChangeDetectionStrategy, Component, input } from '@angular/core';
 
+/**
+ * A component that renders a GOV.UK back link.
+ *
+ * This component implements the GOV.UK Design System back link component.
+ * It provides a consistent way to help users navigate back to the previous page.
+ *
+ * @example
+ * ```html
+ * <ngx-govuk-back-link></ngx-govuk-back-link>
+ * ```
+ *
+ * @example With inverse color scheme
+ * ```html
+ * <ngx-govuk-back-link [inverse]="true"></ngx-govuk-back-link>
+ * ```
+ *
+ * @property {boolean} inverse - When true, displays the back link in inverse colors
+ *                              for use on darker backgrounds. Defaults to false.
+ *
+ * @see {@link https://design-system.service.gov.uk/components/back-link/ GOV.UK Back link}
+ */
 @Component({
   selector: 'ngx-govuk-back-link',
   standalone: true,
+  imports: [NgClass],
   templateUrl: './back-link.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class GovUKBackLinkComponent {}
+export class GovUKBackLinkComponent {
+  readonly inverse = input(false);
+}
